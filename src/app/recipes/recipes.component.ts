3f6ac132481ce1e6138a1956f41082b162064b59
Evan Haggard <61989628+flyaway12345@users.noreply.github.com>
2023-10-25 00:18:13 -0400

    no

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -8,8 +8,8 @@ import { HttpClient, HttpClientModule } from '@angular/common/http'
 export class RecipesComponent implements OnInit{
 
   recipeData: any;
-  url: string = './assets/recipes.json';
-  // url: string = 'https://raw.githubusercontent.com/flyaway12345/CookingByApproximation/main/src/assets/recipes.json';
+  // url: string = './assets/recipes.json';
+  url: string = 'https://raw.githubusercontent.com/flyaway12345/CookingByApproximation/main/src/assets/recipes.json';
   public constructor(private http: HttpClient) {}
 
 
